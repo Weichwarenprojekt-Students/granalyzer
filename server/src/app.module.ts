bac4813e29dba7cd7ef275c4b413cba5bca255b8
MaxPeltzer <maximilian.peltzer@gmail.com>
2021-03-23 12:55:45 +0100

    feat(backend): Add startup check for tool/customer db - GRAN-3/GRAN-34

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
-import { Neo4jModule } from "nest-neo4j/dist";
+import { Neo4jModule, Neo4jService } from "nest-neo4j/dist";
 
 @Module({
     imports: [
@@ -18,4 +18,16 @@ import { Neo4jModule } from "nest-neo4j/dist";
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    constructor(private neo4jService: Neo4jService) {
+        this.initDatabase();
+    }
+
+    /**
+     * On startup check if the necessary tool and customer DB exists and create them if not
+     */
+    initDatabase() {
+        this.neo4jService.write("CREATE DATABASE tool IF NOT EXISTS").catch(console.error);
+        this.neo4jService.write("CREATE DATABASE customer IF NOT EXISTS").catch(console.error);
+    }
+}
