3d31fa75f56469bd5b78772d6cd62fd672752c78
Stefan Rink <rinkyy.sr@googlemail.com>
2021-03-24 08:52:18 +0100

    test(backend): Create test utils to share necessary methods - GRAN-3

--- server/src/diagrams/diagrams.repository.ts
+++ server/src/diagrams/diagrams.repository.ts
@@ -1,35 +1,12 @@
 import * as neo4j from "neo4j-driver";
-import Result from "neo4j-driver/types/result";
-import ResultSummary from "neo4j-driver/types/result-summary";
-import { QueryResult } from "neo4j-driver";
+import { TestUtils } from "../../test/test-utils";
 
 export class DiagramsRepository {
-    /**
-     * Mock utility
-     * Returns a valid noe4j response from the given data
-     *
-     * @param res
-     * @private
-     */
-    private static mockDbResult(res: any[]): Result {
-        const mock = {
-            records: res.map((row) => ({
-                keys: Object.keys(row),
-                get: (key: string) => (row.hasOwnProperty(key) ? row[key] : null),
-            })),
-            summary: <ResultSummary>{},
-        };
-
-        return <Result>new Promise((resolve) => {
-            resolve(<QueryResult>mock);
-        });
-    }
-
     /**
      * Returns mocked db data
      */
     static mockGetDiagrams() {
-        return this.mockDbResult([
+        return TestUtils.mockDbResult([
             {
                 diagram: {
                     identity: neo4j.int(0),
---
+++ server/test/test-utils.ts
@@ -0,0 +1,26 @@
+import Result from "neo4j-driver/types/result";
+import ResultSummary from "neo4j-driver/types/result-summary";
+import { QueryResult } from "neo4j-driver";
+
+export class TestUtils {
+    /**
+     * Mock utility
+     * Returns a valid noe4j response from the given data
+     *
+     * @param res
+     * @private
+     */
+    static mockDbResult(res: any[]): Result {
+        const mock = {
+            records: res.map((row) => ({
+                keys: Object.keys(row),
+                get: (key: string) => (row.hasOwnProperty(key) ? row[key] : null),
+            })),
+            summary: <ResultSummary>{},
+        };
+
+        return <Result>new Promise((resolve) => {
+            resolve(<QueryResult>mock);
+        });
+    }
+}
