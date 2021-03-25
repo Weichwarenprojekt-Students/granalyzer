968439f07ee9ce7a4919137c9611049ef6d6a71f
Stefan Rink <rinkyy.sr@googlemail.com>
2021-03-25 13:40:38 +0100

    feat(backend): Add swagger docs - GRAN-3

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle("Granalyzer API")
+        .setDescription("Awesome access to the Graph")
+        .setVersion("1.0")
+        .addTag("cats")
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("docs", app, document);
+
     app.setGlobalPrefix("api");
+
     await app.listen(3000);
 }
 
