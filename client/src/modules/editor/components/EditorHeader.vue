d39049b99b7a63117b16e94d2d104c7af9ec1c82
Luis Moser <moser@mail.hs-ulm.de>
2021-04-29 20:10:21 +0200

    feat(client): Add diagram SVG export - GRAN-69

--- client/src/modules/editor/components/EditorHeader.vue
+++ client/src/modules/editor/components/EditorHeader.vue
@@ -1,13 +1,24 @@
 <template>
     <div class="header">
         <h3>{{ $store.state.editor.diagram?.name }}</h3>
+        <button class="btn btn-secondary" @click="exportDiagram">{{ $t("editor.exportDiagram") }}</button>
     </div>
 </template>
 
 <script>
-export default {
+import { defineComponent } from "vue";
+import { saveAs } from "file-saver";
+import { diagramToSVG } from "@/utility";
+
+export default defineComponent({
     name: "EditorHeader",
-};
+    methods: {
+        exportDiagram() {
+            this.$store.commit("editor/centerContent");
+            saveAs(diagramToSVG(document.querySelector("#joint svg")), `${this.$store.state.editor.diagram.name}.svg`);
+        },
+    },
+});
 </script>
 
 <style lang="less" scoped>
@@ -15,5 +26,11 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+    position: relative;
+}
+
+button {
+    position: absolute;
+    right: 16px;
 }
 </style>
