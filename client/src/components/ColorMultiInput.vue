a9f8a97edb125a9dc002ae1a51790a4910688229
Tim Niederer <niederer.tim@gmail.com>
2021-04-29 17:18:53 +0200

    fix(client): Prevent hex color shorthands and alpha channels from destroying the color picker

--- client/src/components/ColorMultiInput.vue
+++ client/src/components/ColorMultiInput.vue
@@ -2,7 +2,7 @@
     <div class="color-input">
         <ColorPicker v-model="color" defaultColor="#FFFFFF" format="hex" :disabled="disabled" />
         <label class="color-label">
-            <input :class="{ 'color-error': error }" v-model="color" maxlength="9" type="text" :disabled="disabled" />
+            <input :class="{ 'color-error': error }" v-model="color" maxlength="7" type="text" :disabled="disabled" />
         </label>
     </div>
 </template>
@@ -27,25 +27,51 @@ export default defineComponent({
     },
     data() {
         return {
+            // The color value
             color: "#FFFFFF",
+            // True if the input color is invalid
             error: false,
+            // True if the new color value should be pushed
+            publish: true,
         };
     },
     mounted() {
-        this.color = this.modelValue;
+        this.transformInput();
     },
     watch: {
+        /**
+         * Watch for color changes from the outside
+         */
         modelValue() {
-            this.color = this.modelValue;
+            this.transformInput();
         },
+        /**
+         * Watch for input changes
+         */
         color() {
             // Correct the color value
             if (!this.color.startsWith("#")) this.color = `#${this.color}`;
             this.color = this.color.toUpperCase();
 
             // Emit the new value (for v-model)
-            this.error = !isHexColor(this.color);
-            if (!this.error) this.$emit("update:modelValue", this.color);
+            this.error = !isHexColor(this.color) || this.color.length < 7;
+            if (!this.error && this.publish) this.$emit("update:modelValue", this.color);
+            this.publish = true;
+        },
+    },
+    methods: {
+        /**
+         * Transform incoming shorthand hex colors into normal hex strings
+         * and cut the alpha channel
+         */
+        transformInput(): void {
+            let validColor = "#";
+            if (this.modelValue.length < 7)
+                for (let i = 1; i < this.modelValue.length; i++)
+                    validColor += this.modelValue.charAt(i) + this.modelValue.charAt(i);
+            else validColor = this.modelValue;
+            this.color = validColor.substr(0, 7);
+            this.publish = false;
         },
     },
 });
