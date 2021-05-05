0d526b64fe5fdcddecba9d82c907811bfb35615d
Philipp Scheede <p.scheede@gmx.de>
2021-05-05 20:07:58 +0200

    fix(client): Save heatmap less often - GRAN-239

--- client/src/modules/editor/modules/heat-map/components/HeatConfigurator.vue
+++ client/src/modules/editor/modules/heat-map/components/HeatConfigurator.vue
@@ -67,14 +67,7 @@ export default defineComponent({
             async handler() {
                 await this.$store.dispatch("editor/updateHeatColors");
 
-                // Only save changes once ever 0.5 seconds
-                if (!this.saveChangeTimeoutActive) {
-                    this.saveChangeTimeoutActive = true;
-                    setTimeout(async () => {
-                        this.saveChangeTimeoutActive = false;
-                        await this.$store.dispatch("editor/saveChange");
-                    }, 500);
-                }
+                this.save();
             },
             deep: true,
         },
@@ -93,16 +86,34 @@ export default defineComponent({
          */
         "$store.state.editor.graphEditor.graphHandler.heatConfigs": {
             async handler() {
+                await this.$store.dispatch("editor/updateHeatColors");
                 // Get changed heat config
                 const heatConfig = this.$store.state.editor.graphEditor.graphHandler?.heatConfigs.get(this.label.name);
+
                 if (heatConfig) {
+                    if (heatConfig === this.heatConfig) return;
                     this.heatConfig = heatConfig;
                 } else this.heatConfig = {} as HeatConfig;
 
                 // Save changes
-                await this.$store.dispatch("editor/saveChange");
+                this.save();
             },
             deep: true,
+        },
+    },
+    methods: {
+        /**
+         * Save after changes to the heat configs
+         */
+        save(): void {
+            // Only save changes once ever 0.5 seconds
+            if (!this.saveChangeTimeoutActive) {
+                this.saveChangeTimeoutActive = true;
+                setTimeout(async () => {
+                    await this.$store.dispatch("editor/saveChange");
+                    this.saveChangeTimeoutActive = false;
+                }, 500);
+            }
         },
     },
 });
